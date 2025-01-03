5a4ab6662318ac9f6b7f0b821f702e45afbd654a
Asgard09 <thanhphuc04nh@gmail.com>
2025-01-03 17:44:08 +0700

    Create Payment Intent Service

--- src/main/java/com/example/AsgardShop/service/PaymentService.java
+++ src/main/java/com/example/AsgardShop/service/PaymentService.java
@@ -1,11 +1,20 @@
 package com.example.AsgardShop.service;
 
+import com.example.AsgardShop.dto.request.PaymentInfoDTO;
 import com.example.AsgardShop.repository.PaymentRepository;
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +26,17 @@ public class PaymentService {
     public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.secret.key}") String secretKey) {
         this.paymentRepository = paymentRepository;
         Stripe.apiKey = secretKey;
+    }
+
+    public PaymentIntent createPaymentIntent(PaymentInfoDTO paymentInfoDTO) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoDTO.getAmount());
+        params.put("currency", paymentInfoDTO.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 }
